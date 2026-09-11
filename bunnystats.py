e0c814bc06bd5aaa0298267416a611431ed7255d
Michael Herrmann <michael@herrmann.io>
2026-09-11 19:46:33 +0200

    Support API keys in get_transfer_bytes_per_url
    They need to be used when 2FA is enabled on the account.

--- bunnystats.py
+++ bunnystats.py
@@ -21,12 +21,20 @@ def get_total_transfer_bytes(pull_zone_id, from_date, to_date_incl, api_key):
     resp.raise_for_status()
     return resp.json()['TotalBandwidthUsed']
 
-def get_transfer_bytes_per_url(pull_zone_id, date, auth_token):
+def get_transfer_bytes_per_url(
+    pull_zone_id, date, auth_token=None, api_key=None
+):
     # N.B. This significantly under-reports traffic because Bunny's logging API
     # does not return data for all requests.
+    if api_key is not None:
+        headers = {'AccessKey': api_key}
+    elif auth_token is not None:
+        headers = {'Authorization': auth_token}
+    else:
+        raise ValueError('Either auth_token or api_key must be given')
     date_str = date.strftime('%m-%d-%y')
     url = f'https://logging.bunnycdn.com/{date_str}/{pull_zone_id}.log'
-    resp = requests.get(url, headers={'Authorization': auth_token})
+    resp = requests.get(url, headers=headers)
     if resp.status_code == 404:
         # This happens when there is no data for the given day. For example,
         # when the pull zone did not yet exist.
